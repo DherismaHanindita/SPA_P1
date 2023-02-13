878f6a164491d05d1f75a5012409fb41671edd54
= <=>
2023-02-13 11:56:11 +0700

    Component

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
 const root = document.getElementById("root");
  
-function Navbar() {
-    const linkHome = document.createElement("a");
-    linkHome.href = "#home";
-    linkHome.textContent = "Home";
-    linkHome.onclick = function (event){
+function Link(props){
+    const link = document.createElement("a");
+    link.href = props.href;
+    link.textContent = props.label;
+    link.onclick = function (event){
         event.preventDefault();
-        const homeScreen = HomeScreen();
+        const component = props.Component();
         root.innerHTML = "";
-        root.append(homeScreen);
+        root.append(component);
         history.pushState(null, "", event.target.href)
     };
 
-    const linkAbout = document.createElement("a");
-    linkAbout.href = "#about";  
-    linkAbout.textContent = "About";
-    linkAbout.onclick = function (event){
-        event.preventDefault();
-        const aboutSreen = AboutSreen();
-        root.innerHTML = "";
-        root.append(aboutSreen);
-        history.pushState(null, "", event.target.href)
-    };
+    return link;
+}
+
+function Navbar() {
+    const linkHome = Link({
+        href: "#home",
+        label: "Home",
+        Component: HomeScreen
+    }); 
+
+    const linkAbout = Link({
+        href: "#about",
+        label: "About",
+        Component: AboutScreen,
+    });
 
     const div = document.createElement("div");
     div.append(linkHome);
@@ -30,18 +35,13 @@ function Navbar() {
     return div;
 }
 
-function AboutSreen() {
-    const linkHome = document.createElement("a");
-    linkHome.href = "#home";
-    linkHome.textContent = "Kembali ke Home";
-    linkHome.onclick = function (event){
-        event.preventDefault();
-        const homeScreen = HomeScreen();
-        root.innerHTML = "";
-        root.append(homeScreen);
-        history.pushState(null, "", event.target.href)
-    };
-
+function AboutScreen() {
+    const linkHome = Link({
+        href: "#home",
+        label: "Kembali Ke Home",
+        Component: HomeScreen
+    });  
+ 
     const text = document.createElement("p");
     text.textContent = "Welcome To About";
 
@@ -72,9 +72,9 @@ function HomeScreen() {
 }
 
 if (location.hash === "#about"){
-    const aboutSreen = AboutSreen();
+    const aboutScreen = AboutScreen();
     root.innerHTML = "";
-    root.append(aboutSreen);
+    root.append(aboutScreen);
 } else if (location.hash === "#home"){
     const homeScreen = HomeScreen();
     root.innerHTML = "";
